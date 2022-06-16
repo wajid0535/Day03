f9bbb0027e213995b8beac13ff27f10e67e9e1e1
Mohd Wajid <107199961+wajid0535@users.noreply.github.com>
2022-06-17 02:20:06 +0530

    Add files via upload
    Resume data in JSON format.

---
+++ script.js
@@ -0,0 +1,38 @@
+var resume=[
+  {
+  "Basic":"Basic details",
+    "Name": "Mohd Wajid",
+    "Email": "wajid0535@gamil.com",
+    "Phone": +919052458210,
+    "Degree": "M.A",
+  },
+  {
+    "Location": "Location Details",
+      "Address": "Hashamabad",
+      "PostalCode": 500005,
+      "City": "Hyderabad",
+      "State": "Telangana",
+      "Country": "India",
+  },
+  {
+  "Education": "Education details",
+  "Institution": "Manuu",
+      "Department": "Public Administration",
+      "StudyType": "Regular",
+      " Year": 2019,
+      "Percentage": 80.06,
+  },
+  {
+"Skills":"Skills Details",
+      "Name": "c,c++,java",
+      "Level": "Beginner",
+  },
+  {
+  "Languages":"Languages Known",
+      "Name": "English,Urdu,Telugu",
+  },
+  {
+  "Hobbies":"Hobbies Details",
+"Name": "Gardening,Reading,Travelling,",
+}];
+console.log(resume);
